27b7f139c195dcb5415356bf0fe0e1e52b4bdba8
Rohit Sharma <rohit.enggboy@gmail.com>
2020-10-17 13:41:46 +0530

    dev prceeed

--- flashcards.py
+++ flashcards.py
@@ -10,3 +10,13 @@ def welcome():
 @app.route("/date")
 def dateTime():
     return "The time now is "+ str(datetime.now())
+
+counter=0
+
+@app.route("/count_views")
+def count_views():
+
+    global counter
+    counter+=1
+    return "number of times this page is view is  "+ str(counter)
+
